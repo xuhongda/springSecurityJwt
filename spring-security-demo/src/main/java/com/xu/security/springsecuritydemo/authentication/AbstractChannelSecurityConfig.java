8c68e0ab16fdeff03067bfb47bfdaae94403987c
xuhongda <xuhongda7@outlook.com>
2018-09-05 16:32:38 +0800

    短信验证

--- spring-security-demo/src/main/java/com/xu/security/springsecuritydemo/authentication/AbstractChannelSecurityConfig.java
+++ spring-security-demo/src/main/java/com/xu/security/springsecuritydemo/authentication/AbstractChannelSecurityConfig.java
@@ -17,17 +17,17 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 public class AbstractChannelSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
+	protected AuthenticationSuccessHandler myAuthenticationSuccessHandler;
 	
 	@Autowired
-	protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+	protected AuthenticationFailureHandler myAuthenticationFailureHandler;
 	
 	protected void applyPasswordAuthenticationConfig(HttpSecurity http) throws Exception {
 		http.formLogin()
 			.loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
 			.loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
-			.successHandler(imoocAuthenticationSuccessHandler)
-			.failureHandler(imoocAuthenticationFailureHandler);
+			.successHandler(myAuthenticationSuccessHandler)
+			.failureHandler(myAuthenticationFailureHandler);
 	}
 	
 }
--- spring-security-demo/src/main/java/com/xu/security/springsecuritydemo/authentication/mobile/SmsCodeAuthenticationSecurityConfig.java
+++ spring-security-demo/src/main/java/com/xu/security/springsecuritydemo/authentication/mobile/SmsCodeAuthenticationSecurityConfig.java
@@ -23,10 +23,10 @@ import org.springframework.stereotype.Component;
 public class SmsCodeAuthenticationSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 	
 	@Autowired
-	private AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
+	private AuthenticationSuccessHandler myAuthenticationSuccessHandler;
 	
 	@Autowired
-	private AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+	private AuthenticationFailureHandler myAuthenticationFailureHandler;
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
@@ -36,8 +36,8 @@ public class SmsCodeAuthenticationSecurityConfig extends SecurityConfigurerAdapt
 		
 		SmsCodeAuthenticationFilter smsCodeAuthenticationFilter = new SmsCodeAuthenticationFilter();
 		smsCodeAuthenticationFilter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class));
-		smsCodeAuthenticationFilter.setAuthenticationSuccessHandler(imoocAuthenticationSuccessHandler);
-		smsCodeAuthenticationFilter.setAuthenticationFailureHandler(imoocAuthenticationFailureHandler);
+		smsCodeAuthenticationFilter.setAuthenticationSuccessHandler(myAuthenticationSuccessHandler);
+		smsCodeAuthenticationFilter.setAuthenticationFailureHandler(myAuthenticationFailureHandler);
 		
 		SmsCodeAuthenticationProvider smsCodeAuthenticationProvider = new SmsCodeAuthenticationProvider();
 		smsCodeAuthenticationProvider.setUserDetailsService(userDetailsService);
