17edc12595e04815b813f352a568f58aa5cdb683
xuhongda <xuhongda7@outlook.com>
2018-09-10 11:54:33 +0800

    spring oauth2 jwt sso

---
+++ security-sso-server/src/main/java/com/xu/securityssoserver/SecuritySsoServerApplication.java
@@ -0,0 +1,12 @@
+package com.xu.securityssoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySsoServerApplication.class, args);
+    }
+}
