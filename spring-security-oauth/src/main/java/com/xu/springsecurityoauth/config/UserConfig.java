a13a583fe4810b2e27a47cc04ba2fcc492d884da
xuhongda <xuhongda7@outlook.com>
2019-01-31 15:04:26 +0800

    clean

--- spring-security-oauth/src/main/java/com/xu/springsecurityoauth/config/UserConfig.java
+++ spring-security-oauth/src/main/java/com/xu/springsecurityoauth/config/UserConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
