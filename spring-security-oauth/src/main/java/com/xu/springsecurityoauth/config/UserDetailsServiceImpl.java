c09fd3cb98487eb5426f8ff8af491623031ba84e
xuhongda <xuhongda7@outlook.com>
2019-06-05 16:52:17 +0800

    user detail 身份验证

--- spring-security-oauth/src/main/java/com/xu/springsecurityoauth/config/UserDetailsServiceImpl.java
+++ spring-security-oauth/src/main/java/com/xu/springsecurityoauth/config/UserDetailsServiceImpl.java
@@ -2,15 +2,12 @@ package com.xu.springsecurityoauth.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author xuhongda on 2019/1/30
@@ -19,12 +16,12 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  */
 @Slf4j
 @Configuration
-public class UserConfig /*extends WebSecurityConfigurerAdapter*/ {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserConfig(PasswordEncoder passwordEncoder) {
+    public UserDetailsServiceImpl(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -37,28 +34,26 @@ public class UserConfig /*extends WebSecurityConfigurerAdapter*/ {
     }*/
 
     /**
-     * 验证：关键在于注入一个 {@link UserDetailsService} Bean
-     * @return {@link UserDetailsService}
+     * <P>
+     *     org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider#authenticate(org.springframework.security.core.Authentication)
+     *     方法进行验证
+     * </P>
+     * @param id 一个代表身份的唯一ID
+     * @return UserDetails
+     * @throws UsernameNotFoundException ex
      */
-    @Bean
-   /* @Override*/
-    public UserDetailsService userDetailsService() {
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
 
-
-        User.UserBuilder userBuilder = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("123456"));
-
-
-
+        //1. 按userName 查询 数据库找出密码
+        String password = "123456";
 
         UserDetails build = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("123456"))
+                .username(id)
+                .password(passwordEncoder.encode(password))
                 .roles("USER")
                 .build();
         log.info("password = {}", build.getPassword());
-        return new InMemoryUserDetailsManager(build);
+        return build;
     }
-
 }
